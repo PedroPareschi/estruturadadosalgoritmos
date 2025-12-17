4239042b203e8aa2d24743ba742ece5979ad775d
pedropareschi <pedropareschi.dev@gmail.com>
2025-12-16 21:54:09 -0300

    MergeSort

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -13,25 +13,49 @@ public class MergeSort {
 
 
     public static void main(String[] args) {
-        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-
+        mergeSort(intArray, 0, intArray.length);
 
         System.out.println(Arrays.toString(intArray));
     }
 
     public static void mergeSort(int[] input, int start, int end) {
+        // Divide-se o array em arrays menores até termos arrays de um elemento só
         if (end - start < 2) {
             return;
         }
 
         int mid = (start + end) / 2;
+        // Fazendo isso recursivamente, para os arrays da esquerda e da direita
         mergeSort(input, start, mid);
         mergeSort(input, mid, end);
+        // Juntando os arrays de forma ordenada
         merge(input, start, mid, end);
     }
 
     public static void merge(int[] input, int start, int mid, int end) {
+        // Otimização: Se o array já tiver ordenado, pular
+        if (input[mid - 1] <= input[mid]) {
+            return;
+        }
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
 
+        // Criando array com espaço o suficiente para os arrays unidos
+        int[] temp = new int[end - start];
+
+        // transversando o array
+        while (i < mid && j < end) {
+            // Colocando o item no array temporário de forma ordenada
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        // Otimização: os elementos que sobrarem serão jogados para o final do array
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+
+        // Copiando o temp array
+        System.arraycopy(temp, 0, input, start, tempIndex);
     }
 }
