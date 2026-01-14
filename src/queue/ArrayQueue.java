79a536e335fd5bea5e3b3fd2da9074b904091281
pedropareschi <pedropareschi.dev@gmail.com>
2026-01-13 21:32:20 -0300

    ArrayQueue melhorado

--- src/queue/ArrayQueue.java
+++ src/queue/ArrayQueue.java
@@ -2,7 +2,6 @@ package queue;
 
 import utils.Employee;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class ArrayQueue {
@@ -15,11 +14,21 @@ public class ArrayQueue {
     }
 
     public void add(Employee employee) {
-        if (back == queue.length) {
-            queue = Arrays.copyOf(queue, queue.length * 2);
+        if (size() == queue.length - 1) {
+            int numItems = size();
+            Employee[] newArray = new Employee[2 * queue.length];
+            System.arraycopy(queue, front, newArray, 0, queue.length - front);
+            System.arraycopy(queue, 0, newArray, queue.length - front, back);
+            queue = newArray;
+            front = 0;
+            back = numItems;
         }
         queue[back] = employee;
-        back++;
+        if (back < queue.length - 1) {
+            back++;
+        } else {
+            back = 0;
+        }
     }
 
     public Employee remove() {
@@ -30,9 +39,11 @@ public class ArrayQueue {
         Employee employee = queue[front];
         queue[front] = null;
         front++;
-        if (size() == 0){
+        if (size() == 0) {
             front = 0;
             back = 0;
+        } else if (front == queue.length) {
+            front = 0;
         }
         return employee;
     }
@@ -45,12 +56,25 @@ public class ArrayQueue {
     }
 
     public int size() {
-        return back - front;
+        if (front <= back) {
+            return back - front;
+        }
+        return back - front + queue.length;
     }
 
     public void printQueue() {
-        for (int i = front; i < back; i++) {
-            System.out.println(queue[i]);
+        if (front <= back) {
+            for (int i = front; i < back; i++) {
+                System.out.println(queue[i]);
+            }
+        } else {
+            for (int i = front; i < queue.length; i++) {
+                System.out.println(queue[i]);
+
+            }
+            for (int i = 0; i < back; i++) {
+                System.out.println(queue[i]);
+            }
         }
     }
 }
--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -6,23 +6,25 @@ import utils.Employee;
 public class Queue {
 
     public static void main(String[] args) {
-        ArrayQueue stack = new ArrayQueue(3);
+        ArrayQueue queue = new ArrayQueue(4);
         Employee stanMarsh = new Employee("Stan", "Marsh", 123);
         Employee kyleBroflovski = new Employee("Kyle", "Broflovski", 456);
         Employee ericCartman = new Employee("Eric", "Cartman", 789);
         Employee kennyMcCormick = new Employee("Kenny", "McCormick", 1011);
 
-        stack.add(kennyMcCormick);
-        stack.add(ericCartman);
-        stack.add(kyleBroflovski);
-        stack.add(stanMarsh);
+        queue.add(kennyMcCormick);
+        queue.add(ericCartman);
+        queue.remove();
+        queue.add(kyleBroflovski);
+        queue.remove();
+        queue.add(stanMarsh);
+        queue.remove();
+        queue.add(kennyMcCormick);
+        queue.remove();
+        queue.add(ericCartman);
 
-        stack.printQueue();
-        System.out.println("=====================================");
-        System.out.println(stack.remove());
-        System.out.println("=====================================");
-        stack.printQueue();
-        System.out.println("=====================================");
-        System.out.println(stack.peek());
+        queue.printQueue();
+
+
     }
 }
